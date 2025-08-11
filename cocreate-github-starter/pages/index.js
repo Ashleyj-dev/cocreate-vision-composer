c5a8c5d7d83b2fecba8e7ac58e7a8a61b493f558
Ashleyj-dev <ashleyjsneesby@gmail.com>
2025-08-11 13:47:58 +0930

    Update project with latest changes

--- cocreate-github-starter/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Welcome to CoCreate – Vision Composer</h1>
-      <p className="mt-4 text-gray-600">Let's build the future, together.</p>
-    </main>
-  );
-}
